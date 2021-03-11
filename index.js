ebe8d3ba86a93615e20c7e1d41a7521b790fcf68
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-11 13:16:22 -0600

    deploy ready

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 // require your server and launch it here
 const server = require('./api/server')
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server listening on port ${port}`))
